bd56d57988f07f2246090ab81cef21909a87003c
Zsolt Nagy <info@zsoltnagy.eu>
2024-05-22 02:58:31 +0200

    parking lot form controlled components

--- src/Components/ParkingLotForm/ParkingLotForm.jsx
+++ src/Components/ParkingLotForm/ParkingLotForm.jsx
@@ -1,18 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button, FormGroup, Label, Input } from 'reactstrap';
 
 import './ParkingLotForm.css';
 
+const PRIORITIES = {
+    Low: 'Low',
+    Medium: 'Medium',
+    High: 'High',
+};
+
 export default function ParkingLotForm() {
+
+    const [date, setDate] = useState('');
+    const [link, setLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [priority, setPriority] = useState(PRIORITIES.Medium);
+
+    function handleDateChange(e) {
+        setDate(e.target.value);
+    }
+
+    function handleLinkChange(e) {
+        setLink(e.target.value);
+    }
+
+    function handleDescriptionChange(e) {
+        setDescription(e.target.value);
+    }
+
+    function handlePriorityChange(e) {
+        setPriority(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(date, link, description, priority);
+    }
+
+
+
     return (
-        <Form data-bs-theme="dark" className="parking-lot-form">
+        <Form 
+            data-bs-theme="dark" 
+            className="parking-lot-form"
+            onSubmit={handleSubmit}>
             <FormGroup className="parking-lot-row">
                 <Label htmlFor="link-date">Date</Label>
-                <Input id="link-date" name="date" type="date" required />
+                <Input 
+                    id="link-date" 
+                    name="date" 
+                    type="date" 
+                    onChange={handleDateChange}
+                    value={date}
+                    required />
             </FormGroup>
             <FormGroup className="parking-lot-row">
                 <Label htmlFor="link-url">Link</Label>
-                <Input id="link-url" name="url" type="url" required />
+                <Input 
+                    id="link-url" 
+                    name="url" 
+                    type="url" 
+                    onChange={handleLinkChange}
+                    value={link}
+                    required />
             </FormGroup>
             <FormGroup className="parking-lot-row">
                 <Label htmlFor="link-description">Description</Label>
@@ -20,6 +70,8 @@ export default function ParkingLotForm() {
                     id="link-description" 
                     name="description" 
                     type="text" 
+                    onChange={handleDescriptionChange} 
+                    value={description}
                     required
                 />               
             </FormGroup>
@@ -27,21 +79,27 @@ export default function ParkingLotForm() {
                 <Input 
                     name="radio-priority" 
                     type="radio" 
-                    value="High" 
+                    value={PRIORITIES.High} 
+                    checked={priority === PRIORITIES.High}
+                    onChange={handlePriorityChange}
                     id="prio-high" />
                 {' '}
                 <Label htmlFor="prio-high" className="me-3">High</Label>
                 <Input 
                     name="radio-priority" 
                     type="radio" 
-                    value="Medium" 
+                    value={PRIORITIES.Medium}
+                    checked={priority === PRIORITIES.Medium}
+                    onChange={handlePriorityChange}
                     id="prio-medium" />
                 {' '}
                 <Label htmlFor="prio-medium" className="me-3">Medium</Label>
                 <Input 
                     name="radio-priority" 
                     type="radio" 
-                    value="Low" 
+                    value={PRIORITIES.Low}
+                    checked={priority === PRIORITIES.Low}
+                    onChange={handlePriorityChange}
                     id="prio-low" />
                 {' '}
                 <Label htmlFor="prio-low" className="me-3">Low</Label>
